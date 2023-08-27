0e3282da6309762a0fb0818866bb8a590c1e38bc
FarhanAhsram <marsha.farhan@gmail.com>
2023-08-27 11:56:36 +0700

    init

--- Tugas3.js
+++ Tugas3.js
@@ -1,10 +1,24 @@
 function MajoritySweeper(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        const element = array[i];
-        
-    }
-}
+    let hasil = [];
+    let munculTerbanyak = 0;
+    let banyakAngka = [];
 
+    for (let i = 0; i < arr.length; i++) {
+        if (banyakAngka === arr[i]) {
+            banyakAngka = arr[i].banyakAngka++;
+        }    
+    }
+    
+    for (let j = 0; j < banyakAngka.length; j++) {
+        if (banyakAngka[j] > munculTerbanyak) {
+            hasil = munculTerbanyak;
+        }
+    
+    }
+
+    return hasil;
+}
+  
 // TEST CASES
 console.log(MajoritySweeper([9, 1, 1, 1, 1, 1, 3, 3, 4, 2, 5, 5, 5])) // [ 9, 3, 3, 4, 2, 5, 5, 5 ]
 console.log(MajoritySweeper([22, 22, 100, 100, 100, 2000])) // [ 22, 22, 2000 ]
